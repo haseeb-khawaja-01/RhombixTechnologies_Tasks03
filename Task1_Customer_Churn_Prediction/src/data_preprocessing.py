684a08820b5879675c4c2a8bd36ba8e0d15679f6
Haseeb Ahmad Khawaja <haseebahmad70141@gmail.com>
2025-10-04 16:55:46 +0500

    updated

--- Task1_Customer_Churn_Prediction/src/data_preprocessing.py
+++ Task1_Customer_Churn_Prediction/src/data_preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def load_data_from_url(url, save_path=None):
-    """Load dataset from a raw URL (CSV). Optionally save to save_path."""
+    
     df = pd.read_csv(url)
     if save_path:
         df.to_csv(save_path, index=False)
@@ -10,11 +10,12 @@ def load_data_from_url(url, save_path=None):
 
 def basic_cleaning(df):
     df = df.copy()
-    # Example cleaning for Telco dataset
+
     if 'TotalCharges' in df.columns:
         df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-        df['TotalCharges'].fillna(df['TotalCharges'].median(), inplace=True)
-    # Drop customerID if present
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median( ))
+
+
     if 'customerID' in df.columns:
         df.drop(columns=['customerID'], inplace=True)
     return df
